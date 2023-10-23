8ee2d8d99c3879c308ca161446cfd3254a02c3c9
nealith <nealith@neaworld.fr>
2023-10-23 21:18:34 +0200

    Improve project 1 reaction to midi inputs

--- src/project1/mod.rs
+++ src/project1/mod.rs
@@ -1,10 +1,11 @@
-use vulkano_macros::BufferContents;
 use crate::create_project;
 
 static COMPUTED_TIME: Lazy<Arc<Mutex<f32>>> = Lazy::new(||{Arc::new(Mutex::new(0.0))});
 static SIZE_FACTOR: Lazy<Arc<Mutex<f32>>> = Lazy::new(||{Arc::new(Mutex::new(1.0))});
 
-const SPEED : f32 = 0.00001;
+const DEFAULT_SPEED : f32 = 0.00001;
+
+static SPEED: Lazy<Arc<Mutex<f32>>> = Lazy::new(||{Arc::new(Mutex::new(DEFAULT_SPEED))});
 
 static OLD_VELOCITIES: Lazy<Arc<Mutex<Vec<u32>>>> = Lazy::new(||{Arc::new(Mutex::new(vec![0,0,0,0,0,0]))});
 
@@ -13,22 +14,16 @@ create_project!("Project 1", "src/project1/frag.glsl",|_time,delta_time, _notes,
 
     let mut time_l = COMPUTED_TIME.lock().unwrap();
     let mut size_factor_l = SIZE_FACTOR.lock().unwrap();
+    let mut speed_l = SPEED.lock().unwrap();
 
-    let mut speed = SPEED;
-
-    if velocities[1] > 0 {
-        speed = 0.0001;
-    } else if velocities[5] > 0 {
-        speed = 0.00001;
-    }
 
     if velocities[0] > 0 && old_velocities[0] == 0 {
         *size_factor_l = 4.0;
-    } else if velocities[3] > 0 && old_velocities[3] == 0 {
-        *size_factor_l = 0.5;
+    //} else if velocities[3] > 0 && old_velocities[3] == 0 {
+    //    *size_factor_l = 0.5;
     } else if velocities[4] > 0 && old_velocities[4] == 0 {
         *size_factor_l = 0.1;
-    } else if velocities[0] == 0 && velocities[3] == 0 && velocities[4] == 0 {
+    } else if velocities[0] == 0 && velocities[4] == 0 {
         *size_factor_l = 1.0;
     }
     
@@ -36,16 +31,22 @@ create_project!("Project 1", "src/project1/frag.glsl",|_time,delta_time, _notes,
     
     if velocities[2] > 0 && old_velocities[2] == 0 {
         color_factor = 0.0;
-    } 
+    }
 
-    *time_l += delta_time as f32 * speed;
+    if velocities[1] > 0 {
+        *speed_l = 0.0001;
+    } else if velocities[5] > 0 && old_velocities[5] == 0 {
+        *speed_l = 0.0;
+    } else if velocities[0] > 0 && old_velocities[0] == 0{
+        *speed_l = DEFAULT_SPEED;
+    }
+
+    *time_l += delta_time as f32 * *speed_l;
     *time_l = *time_l % 1.0;
 
     for d in 0..5 {
         old_velocities[d] = velocities[d];
     }
-
-    //println!("Debug : {} {} {} {}",*time_l,time_l.round() as u32,base_size,color_factor);
 
     let uniform_data = fs::Data {
         time : *time_l,
